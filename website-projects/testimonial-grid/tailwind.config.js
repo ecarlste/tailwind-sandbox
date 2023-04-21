cae5a4076b4b5274bfc2abf99cb2f87df49a99fd
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-21 14:11:39 -0600

    Created initial testimonial grid project

---
+++ website-projects/testimonial-grid/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  extend: {
+    fontFamily: {
+      sans: ['Barlow Semi Condensed', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
