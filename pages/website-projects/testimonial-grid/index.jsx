98f529dd2e8fe6c6037056a1bbe32fa8df8ea8c9
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-26 14:20:38 -0600

    Added font family style for testimonial grid page

--- pages/website-projects/testimonial-grid/index.jsx
+++ pages/website-projects/testimonial-grid/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import { Barlow_Semi_Condensed } from 'next/font/google';
+
+const barlow = Barlow_Semi_Condensed({});
+
 function TestimonialGridSitePage() {
-  return <div> TestimonialGridSitePage</div>;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${barlow.style.fontFamily};
+        }
+      `}</style>
+      <div> TestimonialGridSitePage</div>
+    </>
+  );
 }
 
 export default TestimonialGridSitePage;
