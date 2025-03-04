e20c4bbcec84bceb0c3c5747d1a16959520b71eb
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-04 09:00:53 -0700

    fix button to use semantic html element

--- app/website-projects/social-links-profile/_components/social-link-button.tsx
+++ app/website-projects/social-links-profile/_components/social-link-button.tsx
@@ -4,13 +4,13 @@ type SocialLinksButtonProps = {
 
 export default function SocialLinkButton({ children }: SocialLinksButtonProps) {
   return (
-    <div
+    <button
       className={
         "flex p-[12px] bg-grey-700 text-white text-sm font-bold w-full rounded-lg justify-center " +
         "hover:bg-green hover:text-grey-900 hover:cursor-pointer"
       }
     >
-      <div>{children}</div>
-    </div>
+      {children}
+    </button>
   );
 }
