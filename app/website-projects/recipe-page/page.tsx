21c4b306948470feec4b9873c425c977ccb9f799
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-04 17:15:53 -0700

    remove nested main tag in recipe page component

--- app/website-projects/recipe-page/page.tsx
+++ app/website-projects/recipe-page/page.tsx
@@ -2,8 +2,8 @@ import Recipe from "./_components/recipe";
 
 export default function RecipePage() {
   return (
-    <main className="flex justify-center min-h-lvh bg-stone-100 p-0 sm:px-[48px] sm:py-[128px]">
+    <div className="flex justify-center min-h-lvh bg-stone-100 p-0 sm:px-[48px] sm:py-[128px]">
       <Recipe />
-    </main>
+    </div>
   );
 }
