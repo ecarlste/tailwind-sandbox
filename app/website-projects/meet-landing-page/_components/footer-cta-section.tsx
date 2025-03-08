9de7de4c00d56a7adee7c57ee0e8dff75521336d
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-07 18:41:25 -0700

    fixed the hero bg color mask and z index

--- app/website-projects/meet-landing-page/_components/footer-cta-section.tsx
+++ app/website-projects/meet-landing-page/_components/footer-cta-section.tsx
@@ -3,12 +3,15 @@ import NumberHeading from "./number-heading";
 
 export default function FooterCtaSection() {
   return (
-    <section className="flex flex-col justify-center items-center gap-[-28px] self-stretch">
+    <section className="flex flex-col justify-center items-center self-stretch">
       <NumberHeading num={2} />
       {/* hero */}
-      <div className="flex flex-col px-8 py-16 items-center gap-16 self-stretch bg-footer-cta">
+      <div className="relative z-[-10] flex flex-col px-8 py-16 items-center gap-16 self-stretch bg-footer-cta -mt-7">
+        {/* overlay */}
+        <div className="absolute inset-0 bg-cyan-600 opacity-85" />
+
         {/* content */}
-        <div className="flex flex-col max-w-[504px] items-center gap-6 self-stretch">
+        <div className="relative flex flex-col max-w-[504px] items-center gap-6 self-stretch">
           <h2 className="text-mobile-preset-2 text-white text-center self-stretch">
             Experience more together
           </h2>
