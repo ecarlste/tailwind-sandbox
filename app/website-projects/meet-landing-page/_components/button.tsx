4f263ebf2024040aa5820a80600149c086a8c712
Erik S. Carlsten <ecarlste@gmail.com>
2025-03-08 20:29:40 -0700

    added appropriate colors for hover on meet buttons

--- app/website-projects/meet-landing-page/_components/button.tsx
+++ app/website-projects/meet-landing-page/_components/button.tsx
@@ -9,9 +9,9 @@ export default function Button({ children, variant = "primary" }: ButtonProps) {
   return (
     <button
       className={`
-        flex flex-col px-10 py-4 items-start gap-2 text-white text-preset-5 rounded-[29px]
+        flex flex-col px-10 py-4 items-start gap-2 text-white text-preset-5 rounded-[29px] hover:cursor-pointer
         ${variant === "primary" ? "bg-cyan-600" : "bg-purple-600"}
-        ${variant === "primary" ? "hover:bg-slate-800" : "hover:bg-slate-500"}
+        ${variant === "primary" ? "hover:bg-cyan-400" : "hover:bg-purple-400"}
       `}
     >
       {children}
