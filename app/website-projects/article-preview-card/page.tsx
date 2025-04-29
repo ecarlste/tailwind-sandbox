83299ec78fd82ab3dc7439ba203d4c128d3e9fe9
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-29 12:00:04 -0600

    finished full article preview card layout for desktop no active states

--- app/website-projects/article-preview-card/page.tsx
+++ app/website-projects/article-preview-card/page.tsx
@@ -1,22 +1,37 @@
+import Image from "next/image";
+import IconShare from "./_components/icon-share";
+
 export default function ArticlePreviewCardPage() {
   return (
-    <div className="bg-white rounded-[10px] shadow-card max-w-[730px] gap-10 flex text-grey-900">
-      <div>Image</div>
+    <article className="bg-white rounded-[10px] shadow-card max-w-[730px] gap-10 flex text-grey-900 overflow-hidden">
+      <div className="shrink-0 w-[285px] h-70 bg-image" />
       <div className="flex flex-col gap-6 pr-10 py-[30px]">
-        <div className="text-preset-1">
+        <h1 className="text-preset-1">
           Shift the overall look and feel by adding these wonderful touches to
           furniture in your home
-        </div>
-        <div>
+        </h1>
+        <p className="text-preset-2 text-grey-500">
           Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I’ve got some simple tips to help
-          you make any room feel complete.
-        </div>
-        <div className="flex justify-between">
-          <div>Profile/Date</div>
-          <div>Share Button</div>
+          felt slightly bare and uninviting. I&apos;ve got some simple tips to
+          help you make any room feel complete.
+        </p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <Image
+              src="/images/article-preview-card/avatar-michelle.jpg"
+              alt="Article Author Avatar Image"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <div className="flex flex-col text-preset-2">
+              <b>Michelle Appleton</b>
+              <span className="text-grey-400">28 Jun 2020</span>
+            </div>
+          </div>
+          <IconShare />
         </div>
       </div>
-    </div>
+    </article>
   );
 }
