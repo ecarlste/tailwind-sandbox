8f67ca492d369de6c807c7e62bfde166726745b7
Erik S. Carlsten <ecarlste@gmail.com>
2025-05-02 09:54:04 -0600

    switch call to action div to section

--- app/website-projects/newsletter-signup/page.tsx
+++ app/website-projects/newsletter-signup/page.tsx
@@ -11,7 +11,7 @@ export default function NewsletterSignupPage() {
     <div className="bg-white flex flex-col gap-10">
       <div>Hero Image</div>
       <div className="flex flex-col gap-10 mx-6">
-        <div className="flex flex-col gap-6">
+        <section className="flex flex-col gap-6">
           <h1 className="text-preset-1m">Stay Updated!</h1>
           <p className="text-preset-2">
             Join 60,000+ product managers receiving monthly updates on:
@@ -24,7 +24,7 @@ export default function NewsletterSignupPage() {
               </li>
             ))}
           </ul>
-        </div>
+        </section>
         <form className="flex flex-col gap-6">
           <div className="flex flex-col gap-2">
             <label>Email address</label>
