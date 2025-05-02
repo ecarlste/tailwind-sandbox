18bc475c2a329d9c80c24a76ed0f8624f7bdd756
Erik S. Carlsten <ecarlste@gmail.com>
2025-05-02 09:53:07 -0600

    finished styling benefit list section

--- app/website-projects/newsletter-signup/page.tsx
+++ app/website-projects/newsletter-signup/page.tsx
@@ -1,15 +1,28 @@
+import IconCheck from "./_components/icon-check";
+
+const benefits = [
+  "Product discovery and building what matters",
+  "Measuring to ensure updates are a success",
+  "And much more!",
+];
+
 export default function NewsletterSignupPage() {
   return (
     <div className="bg-white flex flex-col gap-10">
       <div>Hero Image</div>
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-10 mx-6">
         <div className="flex flex-col gap-6">
           <h1 className="text-preset-1m">Stay Updated!</h1>
-          <p>Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul>
-            <li>Product discovery and building what matters</li>
-            <li>Measuring to ensure updates are a success</li>
-            <li>And much more!</li>
+          <p className="text-preset-2">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+          <ul className="flex flex-col gap-2 text-preset-2">
+            {benefits.map((benefit, index) => (
+              <li key={index} className="flex gap-4">
+                <IconCheck />
+                <span>{benefit}</span>
+              </li>
+            ))}
           </ul>
         </div>
         <form className="flex flex-col gap-6">
