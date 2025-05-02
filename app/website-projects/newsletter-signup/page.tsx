c4de1f24ddb733b4ab1e0198dafe213850cba755
Erik S. Carlsten <ecarlste@gmail.com>
2025-05-02 09:41:19 -0600

    finished general layout other than hero image

--- app/website-projects/newsletter-signup/page.tsx
+++ app/website-projects/newsletter-signup/page.tsx
@@ -3,7 +3,15 @@ export default function NewsletterSignupPage() {
     <div className="bg-white flex flex-col gap-10">
       <div>Hero Image</div>
       <div className="flex flex-col gap-10">
-        <h1 className="text-preset-1m">Stay Updated!</h1>
+        <div className="flex flex-col gap-6">
+          <h1 className="text-preset-1m">Stay Updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul>
+            <li>Product discovery and building what matters</li>
+            <li>Measuring to ensure updates are a success</li>
+            <li>And much more!</li>
+          </ul>
+        </div>
         <form className="flex flex-col gap-6">
           <div className="flex flex-col gap-2">
             <label>Email address</label>
