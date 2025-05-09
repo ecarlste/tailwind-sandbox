362da3ebef0eb7a355be1879ab7824c4dd1087aa
Erik S. Carlsten <ecarlste@gmail.com>
2025-05-09 10:01:56 -0600

    initial tip-calculator project and styles

---
+++ app/website-projects/tip-calculator/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import "./tip-calculator.css";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Tip Calculator App",
+  description: "Demo of a React Tip Calculator App.",
+  icons: {
+    icon: "/images/tip-calculator/favicon-32x32.png",
+  },
+};
+
+export default function TipCalculatorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <main className="">{children}</main>;
+}
---
+++ app/website-projects/tip-calculator/page.tsx
@@ -0,0 +1,3 @@
+export default function TipCalculatorPage() {
+  return <main>Tip Calculator</main>;
+}
