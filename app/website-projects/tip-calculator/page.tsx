854318f0e866db2b8ca7f70e08e2c761f6934938
Erik S. Carlsten <ecarlste@gmail.com>
2025-05-19 13:57:50 -0600

    added logo to tip-calculator

--- app/website-projects/tip-calculator/page.tsx
+++ app/website-projects/tip-calculator/page.tsx
@@ -1,13 +1,15 @@
 import ResultsSection from "@/app/website-projects/tip-calculator/_components/summary/ResultsSection";
 import BillSection from "@/app/website-projects/tip-calculator/_components/bill/BillSection";
+import Logo from "./_components/Logo";
 
 export default function TipCalculatorPage() {
   return (
-    <div
-      className={"bg-white rounded-[25px] px-10 py-8 flex gap-12 w-fit h-fit"}
-    >
-      <BillSection />
-      <ResultsSection />
+    <div className="flex flex-col gap-22 items-center h-fit">
+      <Logo />
+      <div className={"bg-white rounded-[25px] px-10 py-8 flex gap-12"}>
+        <BillSection />
+        <ResultsSection />
+      </div>
     </div>
   );
 }
