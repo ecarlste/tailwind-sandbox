5c5b4921791269401b6eb4d82694ea7148a31511
Erik S. Carlsten <ecarlste@gmail.com>
2025-05-19 13:48:12 -0600

    finished implementing custom amount for tip calculator

--- app/website-projects/tip-calculator/_components/bill/TipSelectionSection.tsx
+++ app/website-projects/tip-calculator/_components/bill/TipSelectionSection.tsx
@@ -7,14 +7,28 @@ import {
   TipSelectionAmount,
   tipSelectionAmounts,
 } from "@/app/website-projects/tip-calculator/_types/TipSelectionAmount";
+import Input from "../forms/Input";
+import { useState } from "react";
 
 export default function TipSelectionSection({}) {
   const { tipPercentage, setTipPercentage } = useCalculatorStore(
     (state) => state
   );
+  const [useCustomPercentage, setUseCustomPercentage] = useState(false);
 
-  const handleClick = (amount: TipSelectionAmount) => {
+  const handleTipPercentageClick = (amount: TipSelectionAmount) => {
+    setUseCustomPercentage(false);
     setTipPercentage(amount);
+  };
+
+  const handleCustomPercentageClick = () => {
+    setUseCustomPercentage(true);
+  };
+
+  const handleCustomPercentageChange = (
+    e: React.FormEvent<HTMLInputElement>
+  ) => {
+    setTipPercentage(+e.currentTarget.value);
   };
 
   return (
@@ -24,20 +38,33 @@ export default function TipSelectionSection({}) {
         {tipSelectionAmounts.map((amount) => (
           <Button
             key={amount}
-            className={"w-[116px]"}
-            variant={amount === tipPercentage ? "secondary" : "primary"}
-            onClick={() => handleClick(amount)}
+            className={"w-[116px] "}
+            variant={
+              amount === tipPercentage && !useCustomPercentage
+                ? "secondary"
+                : "primary"
+            }
+            onClick={() => handleTipPercentageClick(amount)}
           >
             {amount}%
           </Button>
         ))}
-        <Button
-          className={"w-[116px"}
-          variant={"tertiary"}
-          onClick={() => handleClick(null)}
-        >
-          Custom
-        </Button>
+        {(!useCustomPercentage && (
+          <Button
+            className={"w-[116px]"}
+            variant={"tertiary"}
+            onClick={handleCustomPercentageClick}
+          >
+            Custom
+          </Button>
+        )) || (
+          <Input
+            className="w-[116px]"
+            inputClassName="py-1"
+            value={`${tipPercentage}`}
+            onChange={handleCustomPercentageChange}
+          />
+        )}
       </div>
     </div>
   );
--- app/website-projects/tip-calculator/_components/forms/Input.tsx
+++ app/website-projects/tip-calculator/_components/forms/Input.tsx
@@ -1,26 +1,32 @@
 import React from "react";
 import Label from "./Label";
+import { cn } from "@/utils/utils";
 
 type InputProps = {
+  className?: string;
   children?: React.ReactNode;
-  label: string;
+  inputClassName?: string;
+  label?: string;
   value: string;
   onChange: (event: React.FormEvent<HTMLInputElement>) => void;
 };
 
 export default function Input({
+  className,
   children,
+  inputClassName,
   label,
   value,
   onChange,
 }: InputProps) {
   return (
-    <div className={"flex flex-col gap-2"}>
-      <Label>{label}</Label>
+    <div className={cn("flex flex-col gap-2", className)}>
+      {label && <Label>{label}</Label>}
       <div
-        className={
-          "flex border-2 border-white focus-within:border-green-400 px-4 py-2 items-center bg-grey-50 rounded-[5px] mx-[-2px]"
-        }
+        className={cn(
+          "flex border-2 border-grey-50 focus-within:border-green-400 items-center bg-grey-50 rounded-[5px] px-4 py-2",
+          inputClassName
+        )}
       >
         {children}
         <input
